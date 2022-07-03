2d9350d896c4156193cf7dfb9950a8f4036704a8
DESKTOP-049KG0F\chris <xyris-info@gmx.de>
2022-07-03 23:31:59 +0200

    projekt angelegt

---
+++ Globals.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var infoActive = false
+
+var spielerLeben = 3
+
+var doubleJump = false
+var twister = false
+var fireBall = false
+
+var portalTutorial = true
+var portalTetris = false
+var portalGras = false
+var portalDungeon = false
+var portalBoss = false
+var portalPortalRoom = false
+
+# Alle konsumebals 
+
+var herz = false
+var zwiebel = false
+var schuhe = false
+var chilli = false
+
+# Gegner Globals
+
+
+func _ready():
+	pass
